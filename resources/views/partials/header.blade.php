2d8be77b54d4594729904ca7bf23d7c51dbc6947
federicaarosio <arosio.fede@gmail.com>
2024-02-05 17:19:50 +0100

    working on links in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,12 @@
     <section class="header-navigation">
         <nav class="navigation">
             <ul class="d-flex gap-4 navigation-menu">
-                <li>Characters</li>
-                <li class="active">Comics</li>
+                <li>
+                    <a href="{{route('comics.index')}}">
+                    Home
+                    </a>
+                </li>
+                <li class="active"><a href="">Comics</a></li>
                 <li>Movies</li>
                 <li>Tv</li>
                 <li>Games</li>
