c02fd8110e0a1193cb120dfa78e1aeb3eb5301cc
federicaarosio <arosio.fede@gmail.com>
2024-02-06 18:35:36 +0100

    implemented index method in MovieController with data from $movies

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -11,7 +12,8 @@ class MovieController extends Controller
      */
     public function index()
     {
-        return view('movies.index');
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
     }
 
     /**
