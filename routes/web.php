317fa4786ab3644ad0fdb87fe1ab78398811c5cb
federicaarosio <arosio.fede@gmail.com>
2024-02-06 14:50:32 +0100

    added routes for both create and store methods for Comic resource

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/', function () {
 // Route::resource('comics', ComicController::class);
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+Route::post('comics', [ComicController::class, 'store'])->name('comics.store');
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
