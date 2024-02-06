0076ceed9c1b836b49ceb799aa1286f2062ad352
federicaarosio <arosio.fede@gmail.com>
2024-02-06 17:20:17 +0100

    added route for index method for Movie resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComicController as ComicController;
+use App\Http\Controllers\MovieController as MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,6 @@ Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::post('comics', [ComicController::class, 'store'])->name('comics.store');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
+
+//Movie
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
