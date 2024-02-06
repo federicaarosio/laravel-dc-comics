c94fdc45d0542b966cf8b9cd0bcd79af1fa34708
federicaarosio <arosio.fede@gmail.com>
2024-02-06 18:28:37 +0100

    fixed movies.php

--- config/movies.php
+++ config/movies.php
@@ -1,29 +1,30 @@
 <?php
 
-return [ 'movie' => 
-    [
-        "title" => "Batman Begins",
-        "description" => "The film reboots the Batman film series, telling the origin story of Bruce Wayne from the death of his parents to his journey to become Batman and his fight to stop Ra's al Ghul and the Scarecrow from plunging Gotham City into chaos.",
-        "src" => "https://m.media-amazon.com/images/I/71BTH8VUA7L._AC_UF1000,1000_QL80_.jpg",
-        "director" => "Christopher Nolan",
+return [
+    'movie' => [
+        [
+            "title" => "Batman Begins",
+            "description" => "The film reboots the Batman film series, telling the origin story of Bruce Wayne from the death of his parents to his journey to become Batman and his fight to stop Ra's al Ghul and the Scarecrow from plunging Gotham City into chaos.",
+            "src" => "https://m.media-amazon.com/images/I/71BTH8VUA7L._AC_UF1000,1000_QL80_.jpg",
+            "director" => "Christopher Nolan",
+        ],
+        [
+            "title" => "X-Men: First Class",
+            "description" => "First Class is set primarily in 1962 during the Cuban Missile Crisis, and focuses on the relationship between Charles Xavier and Erik Lehnsherr / Magneto, and the origin of their groups—the X-Men and the Brotherhood of Mutants, respectively, as they deal with the Hellfire Club led by Sebastian Shaw, a mutant supremacist bent on enacting nuclear war.",
+            "src" => "https://upload.wikimedia.org/wikipedia/en/5/55/X-MenFirstClassMoviePoster.jpg",
+            "director" => "Matthew Vaughn",
+        ],
+        [
+            "title" => "Fantastic Four",
+            "description" => "Fantastic Four (styled as FANT4STIC) is a 2015 superhero film based on the Marvel Comics superhero team of the same name. Directed by Josh Trank, who co-wrote the screenplay with Jeremy Slater and Simon Kinberg, it stars Miles Teller, Michael B. Jordan, Kate Mara, Jamie Bell and Toby Kebbell. The film follows a group of intelligent teenagers that build a transdimensional portal, causing them to gain superhuman abilities.",
+            "src" => "https://upload.wikimedia.org/wikipedia/en/f/f4/Fantastic_Four_2015_poster.jpg",
+            "director" => "Josh Trank",
+        ],
+        [
+            "title" => "Iron Man",
+            "description" => "Iron Man is a 2008 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Paramount Pictures,[a] it is the first film in the Marvel Cinematic Universe (MCU). Directed by Jon Favreau from a screenplay by the writing teams of Mark Fergus and Hawk Ostby, and Art Marcum and Matt Holloway, the film stars Robert Downey Jr. as Tony Stark / Iron Man alongside Terrence Howard, Jeff Bridges, Gwyneth Paltrow, Leslie Bibb, and Shaun Toub. In the film, following his escape from captivity by a terrorist group, world-famous industrialist and master engineer Tony Stark builds a mechanized suit of armor and becomes the superhero Iron Man.",
+            "src" => "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg",
+            "director" => "Jon Favreau",
+        ],
     ],
-    [
-        "title" => "X-Men: First Class",
-        "description" => "First Class is set primarily in 1962 during the Cuban Missile Crisis, and focuses on the relationship between Charles Xavier and Erik Lehnsherr / Magneto, and the origin of their groups—the X-Men and the Brotherhood of Mutants, respectively, as they deal with the Hellfire Club led by Sebastian Shaw, a mutant supremacist bent on enacting nuclear war.",
-        "src" => "https://upload.wikimedia.org/wikipedia/en/5/55/X-MenFirstClassMoviePoster.jpg",
-        "director" => "Matthew Vaughn",
-    ],
-    [
-        "title" => "Fantastic Four",
-        "description" => "Fantastic Four (styled as FANT4STIC) is a 2015 superhero film based on the Marvel Comics superhero team of the same name. Directed by Josh Trank, who co-wrote the screenplay with Jeremy Slater and Simon Kinberg, it stars Miles Teller, Michael B. Jordan, Kate Mara, Jamie Bell and Toby Kebbell. The film follows a group of intelligent teenagers that build a transdimensional portal, causing them to gain superhuman abilities.",
-        "src" => "https://upload.wikimedia.org/wikipedia/en/f/f4/Fantastic_Four_2015_poster.jpg",
-        "director" => "Josh Trank",
-    ],
-    [
-        "title" => "Iron Man",
-        "description" => "Iron Man is a 2008 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Paramount Pictures,[a] it is the first film in the Marvel Cinematic Universe (MCU). Directed by Jon Favreau from a screenplay by the writing teams of Mark Fergus and Hawk Ostby, and Art Marcum and Matt Holloway, the film stars Robert Downey Jr. as Tony Stark / Iron Man alongside Terrence Howard, Jeff Bridges, Gwyneth Paltrow, Leslie Bibb, and Shaun Toub. In the film, following his escape from captivity by a terrorist group, world-famous industrialist and master engineer Tony Stark builds a mechanized suit of armor and becomes the superhero Iron Man.",
-        "src" => "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg",
-        "director" => "Jon Favreau",
-    ],
-
 ];
