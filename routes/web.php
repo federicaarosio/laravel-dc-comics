d1215a7521a1751c87191cf45db2da885150ba41
federicaarosio <arosio.fede@gmail.com>
2024-02-07 14:44:44 +0100

    added edit route, fixed show route for Comic resource

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,12 @@ Route::get('/', function () {
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::post('comics', [ComicController::class, 'store'])->name('comics.store');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+
 
 //Movie
 Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
 Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
 Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
-Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
