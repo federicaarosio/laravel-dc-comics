611a9cd488f3c1a0607d97f674802a054c536424
federicaarosio <arosio.fede@gmail.com>
2024-02-07 14:52:07 +0100

    added action route and value to form for Comic resource

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,7 +11,7 @@
 
         <div class="article-container">
 
-            <form action=" " method="POST">
+            <form action="{{ route('comics.update', $comic->id) }}" method="POST">
                 @csrf
 
                 {{-- custom method--}}
@@ -21,49 +21,49 @@
                     <label for="title" class="form-label text-white">
                         Title:
                     </label>
-                    <input type="text" name="title" id="title" class="form-control">
+                    <input type="text" name="title" id="title" class="form-control" value="{{ old( 'title' ,$comic->title) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="description" class="form-label text-white">
                         Description:
                     </label>
-                    <input type="text" name="description" id="description" class="form-control">
+                    <input type="text" name="description" id="description" class="form-control" value="{{ old( 'description' ,$comic->description) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="thumb" class="form-label text-white">
                         Thumb url:
                     </label>
-                    <input type="text" name="thumb" id="thumb" class="form-control">
+                    <input type="text" name="thumb" id="thumb" class="form-control" value="{{ old( 'thumb' ,$comic->thumb) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="price" class="form-label text-white">
                         Price:
                     </label>
-                    <input type="text" name="price" id="price" class="form-control">
+                    <input type="text" name="price" id="price" class="form-control" value="{{ old( 'price' ,$comic->price) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="series" class="form-label text-white">
                         Series:
                     </label>
-                    <input type="text" name="series" id="series" class="form-control">
+                    <input type="text" name="series" id="series" class="form-control" value="{{ old( 'series' ,$comic->series) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="sale_date" class="form-label text-white">
                         Sale date:
                     </label>
-                    <input type="text" name="sale_date" id="sale_date" class="form-control">
+                    <input type="text" name="sale_date" id="sale_date" class="form-control" value="{{ old( 'sale_date' ,$comic->sale_date) }}">
                 </div>
 
                 <div class="mb-3">
                     <label for="type" class="form-label text-white">
                         Type:
                     </label>
-                    <input type="text" name="type" id="type" class="form-control">
+                    <input type="text" name="type" id="type" class="form-control" value="{{ old( 'type' ,$comic->type) }}">
                 </div>
 
                 <div class="button-wrapper">
