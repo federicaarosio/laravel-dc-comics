012528a40c4ccfbc1a745afd27fd44f89de1f4fa
federicaarosio <arosio.fede@gmail.com>
2024-02-07 16:59:34 +0100

    added edit button to show for Movie resource

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -14,9 +14,21 @@
             <article class="card" style="width: 18rem;">
                 <img src="{{ $movie['src'] }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">{{ $movie['title'] }}</h5>
-                  <p>Description: {{ $movie['description'] }}</p>
+                    <h5 class="card-title">{{ $movie['title'] }}</h5>
+                    <p>Description: {{ $movie['description'] }}</p>
+
+                    <div class="actions mb-3 pt-3">
+                        <a href="{{ route('movies.edit', $movie->id) }}">
+                            <button class="btn btn-primary">
+                                Edit this movie
+                            </button>
+                        </a>
+                    </div>
+
                 </div>
+
+
+                
             </article>
         </div>
 
