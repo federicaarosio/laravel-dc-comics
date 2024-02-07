5dccb5197765c595bbadcbf3145dc36383dd7ba2
federicaarosio <arosio.fede@gmail.com>
2024-02-07 14:42:35 +0100

    added edit method in ComicController for Comic resource

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -62,7 +62,8 @@ class ComicController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        return view('comics.edit', compact('comic'));
     }
 
     /**
